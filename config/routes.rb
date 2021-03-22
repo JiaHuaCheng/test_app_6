50a9f4b5544d840e66326349590c1b834e18869d
JiaHuaCheng <jhchenge@gmail.com>
2021-03-22 13:01:17 -0500

    Add about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # add root route
   root 'pages#home'
+  get 'about', to: 'pages#about'
   # root for root space, the quote is to specify where send this to. application stands for application_controller. 
   # we don't need to write down _controller here. Rails will handle it for us.
   # next, we need a method in controller to handle this request. At this step, we need to specify the method name in the controller 
